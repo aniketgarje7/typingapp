9b22e7eb2e747cd32a4905746f3ed4253af5de3e
aniketgarje7 <garjeaniket7@gmail.com>
2023-02-12 09:04:18 +0530

    logged

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,10 +4,13 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
+import Login from './Login'
 
 
 const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box sx={{ flexGrow: 1}}>
     <AppBar position="static">
@@ -16,7 +19,9 @@ const Navbar = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Typing Test
         </Typography>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit" onClick={handleOpen}>logn</Button>
+        <Login handleClose={handleClose}
+         open={open} setOpen={setOpen}/>
       </Toolbar>
     </AppBar>
   </Box>
